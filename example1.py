e5982d4798668c1ab7e8b1b993e29d7b50935656
Rishi Raj <nahsorjar09@gmail.com>
2025-02-25 21:55:07 +0530

    Newest change

---
+++ example1.py
@@ -0,0 +1,69 @@
+##INTEGRATE OUR CODE WITHE OPENAI API KEY
+import os
+import streamlit as st
+from constants import openai_key
+from langchain_openai import ChatOpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+# from langchain.chains import SimpleSequentialChain
+from langchain.chains import SequentialChain
+#for memory-
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+os.environ["OPENAI_API_KEY"] = openai_key
+
+#streamlit framework-
+
+st.title('Celebrity Search Using Langchain')
+input_text = st.text_input("Search")
+
+# Dictionary to store session histories
+session_histories = {}
+
+def get_session_history(session_id: str) -> InMemoryChatMessageHistory:
+    if session_id not in session_histories:
+        session_histories[session_id] = InMemoryChatMessageHistory()
+    return session_histories[session_id]
+
+#Prompt Template-1
+first_input_prompt=PromptTemplate(input_variables=['name'],template="Tell me about celebrity {name}")
+
+##1st part of chain of LLM or we can say the first chain of LLM-
+llm = ChatOpenAI(temperature=0.8)
+
+# Memory integration using new approach
+llm_with_memory = RunnableWithMessageHistory(llm, get_session_history)
+
+chain=LLMChain(llm=llm_with_memory,prompt=first_input_prompt,verbose=True,output_key='person')#the use of this output_key
+#the use of this output_key is to LABEL this output and send it as a speicific input to the next prompt/prompt tempalte we want
+
+#Prompt Template-2
+second_input_prompt=PromptTemplate(input_variables=['name'],template="When was {name} born")
+
+chain2=LLMChain(llm=llm_with_memory,prompt=second_input_prompt,verbose=True,output_key='dob')#we put label 'dob' on output of 2nd prompt
+
+#Prompt Template-3
+third_input_prompt=PromptTemplate(input_variables=['dob'],template="Give 5 things that happened in the world around {dob}" )
+chain3=LLMChain(llm=llm_with_memory,prompt=third_input_prompt,verbose=True,output_key='description')
+
+# parent_chain=SimpleSequentialChain(chain=[chain,chain2],verbose=True)
+parent_chain=SequentialChain(chain=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'], verbose=True)
+
+if input_text:
+    session_id = "user_session"  # Replace with a unique identifier as needed
+    # st.write(llm(input_text))
+
+    #next format for when SimpleSequentialgame are we-
+
+    # st.write(parent_chain.run(input_text)
+
+    #now,for the 
+    response = parent_chain({'name':input_text})
+    st.write(response)
+    
+    with st.expander('Person Name'): 
+        st.info(get_session_history(session_id).messages)
+
+    with st.expander('Major Events'): 
+        st.info(get_session_history(session_id).messages)
